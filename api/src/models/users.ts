80a688387fc2d7d3f138fbfc47b142e4800e871f
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-03 10:44:50 +0000

    Tested: Users are created and added to the database

---
+++ api/src/models/users.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true } as any,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    dropDups: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+  } as any,
+  password: { type: String, required: true } as any,
+});
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
---
+++ api/src/spec/models/user.test.ts
@@ -0,0 +1,27 @@
+import User, { IUser } from "../../models/users";
+import mongoose from "mongoose";
+import "../mongodb_helper";
+
+describe("User", () => {
+  let user: IUser;
+  beforeEach(async () => {
+    await User.deleteMany();
+    user = await User.create({
+      name: "Robbie",
+      email: "robbie@email.com",
+      password: "password1",
+    });
+  });
+
+  it("has a name, email and password", () => {
+    expect(user.name).toEqual("Robbie");
+    expect(user.email).toEqual("robbie@email.com");
+    expect(user.password).toEqual("password1");
+  });
+
+  it("users are saved to the database and can be accessed", async () => {
+    const users = await User.find();
+    expect(users.length).toEqual(1);
+    expect(users[0].name).toEqual("Robbie");
+  });
+});
