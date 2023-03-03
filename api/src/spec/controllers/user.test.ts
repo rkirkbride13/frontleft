59479a6c2240b860c70715aee15774cf532b7fb7
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-03 14:49:28 +0000

    Test for user controller written, production code not written yet

---
+++ api/src/spec/controllers/user.test.ts
@@ -0,0 +1,28 @@
+import {app} from "../../app";
+import request from "supertest";
+import "../mongodb_helper";
+import User from "../../models/users";
+import bcrypt from "bcrypt";
+
+describe("/users", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  describe("post when email and password are provided", () => {
+    it("gives response code 201 and creates a user", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({
+          name: "Robbie",
+          email: "robbie@email.com",
+          password: "password",
+        });
+        expect(response.statusCode).toBe(201)
+
+        let users = await User.find()
+        let newUser = users[users.length - 1];
+        expect(newUser.email).toEqual("robbie@email.com");
+    });
+  });
+});
