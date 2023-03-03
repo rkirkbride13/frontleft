d05e05e26176cfa9d1dc3ac3f33530a092169587
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-03 15:39:34 +0000

    Tested: User not created if details are invalid or if email exists already

--- api/src/spec/controllers/user.test.ts
+++ api/src/spec/controllers/user.test.ts
@@ -1,4 +1,4 @@
-import {app} from "../../app";
+import { app } from "../../app";
 import request from "supertest";
 import "../mongodb_helper";
 import User from "../../models/users";
@@ -11,33 +11,87 @@ describe("/users", () => {
 
   describe("post when email and password are provided", () => {
     it("gives response code 201 and creates a user", async () => {
-      let response = await request(app)
-        .post("/users")
-        .send({
-          name: "Robbie",
-          email: "robbie@email.com",
-          password: "password",
-        });
-        expect(response.statusCode).toBe(201)
-
-        let users = await User.find()
-        let newUser = users[users.length - 1];
-        expect(newUser.email).toEqual("robbie@email.com");
-    });
-
-    it('encrypts the password', async () => {
-      let response = await request(app)
-      .post("/users")
-      .send({
+      let response = await request(app).post("/users").send({
         name: "Robbie",
         email: "robbie@email.com",
         password: "password",
       });
-      expect(response.statusCode).toBe(201)
+      expect(response.statusCode).toBe(201);
 
-      let users = await User.find()
+      let users = await User.find();
       let newUser = users[users.length - 1];
-      bcrypt.compare("password", newUser.password).then((res) => expect(res).toBe(true))
-    })
+      expect(newUser.email).toEqual("robbie@email.com");
+    });
+
+    it("encrypts the password", async () => {
+      let response = await request(app).post("/users").send({
+        name: "Robbie",
+        email: "robbie@email.com",
+        password: "password",
+      });
+      expect(response.statusCode).toBe(201);
+
+      let users = await User.find();
+      let newUser = users[users.length - 1];
+      bcrypt
+        .compare("password", newUser.password)
+        .then((res) => expect(res).toBe(true));
+    });
+  });
+
+  describe("post when name not provided", () => {
+    it("gives response code 400 and does NOT create user", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({ email: "robbie@email.com", password: "password1" });
+      expect(response.statusCode).toBe(400);
+      let users = await User.find();
+      expect(users.length).toEqual(0);
+    });
+  });
+
+  describe("post when email not provided", () => {
+    it("gives response code 400 and does NOT create user", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({ name: "Robbie", password: "password1" });
+      expect(response.statusCode).toBe(400);
+      let users = await User.find();
+      expect(users.length).toEqual(0);
+    });
+  });
+
+  describe("post when password not provided", () => {
+    it("gives response code 400 and does NOT create user", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({ name: "Robbie", email: "robbie@email.com" });
+      expect(response.statusCode).toBe(400);
+      let users = await User.find();
+      expect(users.length).toEqual(0);
+    });
+  });
+
+  describe("post when email is invalid", () => {
+    it("gives response code 400 and does NOT create user", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({ name: "Robbie", email: "robbieemail.com", password: "password1" });
+      expect(response.statusCode).toBe(400);
+      let users = await User.find();
+      expect(users.length).toEqual(0);
+    });
+  });
+
+  describe("post when email exists in DB", () => {
+    it("gives response code 400 and does NOT create user", async () => {
+      await User.create({name: "Robbie", email: "robbie@email.com", password: "password1"})
+      let response = await request(app)
+        .post("/users")
+        .send({ name: "Robbie New", email: "robbie@email.com", password: "password100" });
+      expect(response.statusCode).toBe(400);
+      let users = await User.find();
+      expect(users.length).toEqual(1);
+    });
   });
 });
