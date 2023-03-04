4de48f67adc8e9b11257d641cbfec23acff97082
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-04 11:46:34 +0000

    Middleware added to app.ts to check for tokens

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,36 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import { config } from 'dotenv';
-import actsRouter from './routes/acts';
-import usersRouter from './routes/users';
-import tokensRouter from './routes/tokens';
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import actsRouter from "./routes/acts";
+import usersRouter from "./routes/users";
+import tokensRouter from "./routes/tokens";
+import JWT, { JwtPayload } from "jsonwebtoken";
 
-config({ path: './config.env' });
-
+config({ path: "./config.env" });
 const app: Express = express();
-
 app.use(cors());
 app.use(express.json());
-app.use('/acts', actsRouter);
-app.use('/users', usersRouter);
-app.use('/tokens', tokensRouter);
 
-export {app};
+// token middleware
+const tokenChecker = (req: Request, res: Response, next: NextFunction) => {
+  let token: string | undefined;
+  const authHeader = req.get("Authorization");
+
+  if (authHeader) {
+    token = authHeader.slice(7);
+  }
+
+  if (token) {
+    const payload = JWT.verify(token, process.env.JWT_SECRET as string) as JwtPayload
+    req.body.user_id = payload.user_id
+    next()
+  } else {
+    res.status(401).json({ message: "Auth error" })
+  }
+};
+
+app.use("/acts", tokenChecker, actsRouter);
+app.use("/users", usersRouter);
+app.use("/tokens", tokensRouter);
+
+export { app };
