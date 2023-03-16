b2672b724c05b0095131099442590cf5db3459dd
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-16 16:55:50 +0000

    ActForm route removed

--- app/src/components/app/App.tsx
+++ app/src/components/app/App.tsx
@@ -1,21 +1,21 @@
 import { ReactElement } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import ActForm from '../ActForm/ActForm';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import SignInForm from "../SignInForm/SignInForm";
 import Acts from '../Acts/userActs'
 import Friday from '../DayCharts/friday'
 import Saturday from '../DayCharts/saturday'
+import Sunday from '../DayCharts/sunday'
 
 const App = (): ReactElement => {
   return (
     <Routes>
-      {/* <Route path="/act" element={<ActForm navigate={useNavigate()}/>} /> */}
       <Route path="/signup" element={<SignUpForm navigate={useNavigate()}/>} />
       <Route path="/" element={<SignInForm navigate={useNavigate()}/>} />
       <Route path="/acts" element={<Acts navigate={useNavigate()}/>} />
       <Route path="/day/friday" element={<Friday navigate={useNavigate()}/>} />
       <Route path="/day/saturday" element={<Saturday navigate={useNavigate()}/>} />
+      <Route path="/day/sunday" element={<Sunday navigate={useNavigate()}/>} />
     </Routes>
   );
 };
