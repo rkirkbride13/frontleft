c45e6da5db3dafdf97e0551c21d954987e2277bd
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-25 16:06:11 +0000

    Tested that an act could be deleted

--- app/src/components/Act/act.cy.tsx
+++ app/src/components/Act/act.cy.tsx
@@ -1,7 +1,7 @@
 import Act from "./act";
 import { IAct } from "../../../../api/src/models/acts";
 
-const act: IAct | any= {
+const act: IAct | any = {
   name: "Robbie",
   stage: "Git staging",
   date: new Date(),
@@ -11,9 +11,26 @@ const act: IAct | any= {
 };
 
 describe("Act", () => {
-  it("renders with an act", () => {
+  it("renders with an act and delete button", () => {
     let setActsMock = cy.stub();
-    cy.mount(<Act act={act} token={"tokenMock"} setActs={setActsMock}/>);
+
+    cy.mount(<Act act={act} token={"tokenMock"} setActs={setActsMock} />);
+
     cy.get('[data-cy="act"]').should("contain.text", "Robbie - 1800 to 1900");
+    cy.get('[data-cy="delete-button"]')
+      .invoke("attr", "type")
+      .should("eq", "submit");
+  });
+
+  it("sends a DELETE request and can delete an act", () => {
+    let setActsMock = cy.stub();
+
+    cy.mount(<Act act={act} token={"tokenMock"} setActs={setActsMock} />);
+    cy.intercept("DELETE", "acts", { message: "DELETED" }).as("deleteAct");
+
+    cy.get('[data-cy="delete-button"]').click();
+    cy.wait("@deleteAct").then((interception) => {
+      expect(interception.response!.body.message).to.eq("DELETED");
+    });
   });
 });
